934b60b8393ab41748f9770482594561e43b03d5
Вадим Семочкин <vadimsemochkin96@yandex.ru>
2022-01-21 22:30:14 +0300

    Добавьте файлы проекта.

---
+++ WPF9/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles; 
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedItem = 0;
+           
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("Light.xaml", UriKind.Relative);
+            if (styleIndex==1)
+            {
+                uri = new Uri("Dark.xaml", UriKind.Relative);
+            }
+           
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BoldExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+                textBox.FontWeight = FontWeights.Bold;
+            else
+                textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void ItalicExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+                textBox.FontStyle = FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void UnderlineExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+                textBox.TextDecorations = null;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //string fontName = ((sender as ComboBox).SelectedItem as ItemCollection).Contains;
+            //if (textBox != null)
+            //    textBox.FontFamily = new FontFamily(fontName);
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            //string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            //if (textBox != null)
+            //    textBox.FontSize = Convert.ToDouble(fontSize);
+        }
+        private void buttonBlack_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Black;
+        }
+
+        private void buttonRed_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Red;
+        }
+    }
+}
---
+++ WPF9/MyCommands.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF9
+{
+    class MyCommands
+    {
+        public static RoutedCommand TextBold { get; set; }
+        public static RoutedCommand TextItalic { get; set; }
+        public static RoutedCommand TextUnderline { get; set; }
+
+
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs1 = new InputGestureCollection();
+            inputs1.Add(new KeyGesture(Key.B, ModifierKeys.Control, "Ctrl+B"));
+
+            InputGestureCollection inputs2 = new InputGestureCollection();
+            inputs2.Add(new KeyGesture(Key.I, ModifierKeys.Control, "Ctrl+I"));
+
+            InputGestureCollection inputs3 = new InputGestureCollection();
+            inputs3.Add(new KeyGesture(Key.U, ModifierKeys.Control, "Ctrl+U"));
+
+            TextBold = new RoutedCommand("Bold", typeof(MyCommands), inputs1);
+            TextItalic = new RoutedCommand("Italic", typeof(MyCommands), inputs2);
+            TextUnderline = new RoutedCommand("Underline", typeof(MyCommands), inputs3);
+        }
+    }
+}
